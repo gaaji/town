306bd8c1f5e1ca858dd7057714e881c321bafa10
ansm6 <ansm6@host.docker.internal>
2023-01-16 13:46:53 +0900

    에러코드 이름 변경

--- src/main/java/com/gaaji/town/exception/TownErrorCode.java
+++ src/main/java/com/gaaji/town/exception/TownErrorCode.java
@@ -9,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum TownErrorCode implements ErrorCode {
 
-	Max_Town_Num(HttpStatus.UNAUTHORIZED,"A-0001", "등록된 동네의 개수가 최대치입니다."),
-	Min_Town_Num(HttpStatus.UNAUTHORIZED,"A-0002", "최소 1개의 동네를 가지고 있어야 합니다."),
-	Overlap_Town(HttpStatus.UNAUTHORIZED,"A-0003", "이미 등록된 동네를 입력했습니다."),
-	No_Serch(HttpStatus.UNAUTHORIZED,"A-0004", "검색된 유저 정보가 존재하지 않습니다."),
+	Max_Town_Num(HttpStatus.UNAUTHORIZED,"T-0001", "등록된 동네의 개수가 최대치입니다."),
+	Min_Town_Num(HttpStatus.UNAUTHORIZED,"T-0002", "최소 1개의 동네를 가지고 있어야 합니다."),
+	Overlap_Town(HttpStatus.UNAUTHORIZED,"T-0003", "이미 등록된 동네를 입력했습니다."),
+	No_Serch(HttpStatus.UNAUTHORIZED,"T-0004", "검색된 유저 정보가 존재하지 않습니다."),
     ;
 	
 	private final HttpStatus httpStatus;
