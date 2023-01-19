206987913e9819f0ab6e3120862d78eee39c27c0
kyoohyun <kgh2120@naver.com>
2023-01-19 17:01:08 +0900

    GM-92 CHORE : 토큰 컨버팅 체크

--- src/main/java/com/gaaji/town/controller/dto/TownToken.java
+++ src/main/java/com/gaaji/town/controller/dto/TownToken.java
@@ -1,10 +1,12 @@
 package com.gaaji.town.controller.dto;
 
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor
-public class TownToken {
+@Data @AllArgsConstructor @NoArgsConstructor
+public class TownToken implements Serializable {
 
     private String townId;
     private String address;
